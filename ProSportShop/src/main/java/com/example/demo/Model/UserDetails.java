7423ee337b353b967b27b4ec095cf77dd4b7ba6b
Harish270703 <128140454+Harish270703@users.noreply.github.com>
2023-08-25 12:29:25 +0530

    Add files via upload

---
+++ ProSportShop/src/main/java/com/example/demo/Model/UserDetails.java
@@ -0,0 +1,65 @@
+package com.example.demo.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="UserDetails")
+public class UserDetails {
+	@Id
+	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String productname;
+	private String price;
+	private String brand;
+	private String size;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getProductname() {
+		return productname;
+	}
+
+	public void setProductname(String productname) {
+		this.productname = productname;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	public UserDetails() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+}
